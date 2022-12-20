d08f2d697d38f6bda1d899896e4a6dfa6ce66167
OleksandrBabichTR <oleksandr.babich@thomsonreuters.com>
2022-12-20 10:15:38 +0100

    Refactoring constructor of product.model.ts

--- src/app/product/models/product.model.ts
+++ src/app/product/models/product.model.ts
@@ -1,13 +1,8 @@
 export class ProductModel {
-  name: string = "Component name";
-  description: string = "Component description";
-  price: number = 0;
-  isAvailable: boolean = true;
-
-  constructor (name: string, description: string, price: number, isAvailable: boolean = true) {
-    this.name = name;
-    this.description = description;
-    this.price = price;
-    this.isAvailable = isAvailable;
-  }
+  constructor (
+    public name: string,
+    public description: string,
+    public price: number  = 0,
+    public isAvailable: boolean = true
+  ) {}
 }
